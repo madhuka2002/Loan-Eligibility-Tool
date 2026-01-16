932364143d5d4d2efa1d7ab7ba5485ad69a7535b
SithilSemitha <sithilsemithad@gmail.com>
2026-01-16 21:26:44 +0530

    feat: Added intial Commit

---
+++ src/main/java/com/loantool/Main.java
@@ -0,0 +1,11 @@
+package com.loantool;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to LoanTool!");
+    }
+}
