58117e420c6b9392f32095647d6f1944135fe83f
SithilSemitha <sithilsemithad@gmail.com>
2026-01-30 22:22:11 +0530

    feat: Added sort array QuickSorter functions

--- src/main/java/com/loantool/algorithms/QuickSorter.java
+++ src/main/java/com/loantool/algorithms/QuickSorter.java
@@ -30,18 +30,18 @@ public class QuickSorter {
             // Partition the array
             int pivotIndex = partition(array, low, high);
 
-            // Recursively sort elements before and after partition
             quickSort(array, low, pivotIndex - 1);
             quickSort(array, pivotIndex + 1, high);
         }
     }
 
     private int partition(Applicant[] array, int low, int high) {
+
         // Randomized pivot selection for better average performance
         Random rand = new Random();
         int pivotIndex = low + rand.nextInt(high - low + 1);
 
-        // Swap pivot with last element
+        // Swap pivot
         Applicant pivot = array[pivotIndex];
         array[pivotIndex] = array[high];
         array[high] = pivot;
@@ -66,6 +66,54 @@ public class QuickSorter {
         array[high] = temp;
 
         return i + 1;
+    }
+
+    public List<Applicant> sortThreeWay(List<Applicant> applicants) {
+        if (applicants.size() <= 1) {
+            return new ArrayList<>(applicants);
+        }
+
+        Applicant[] array = applicants.toArray(new Applicant[0]);
+        quickSortThreeWay(array, 0, array.length - 1);
+
+        List<Applicant> sorted = new ArrayList<>();
+        for (Applicant applicant : array) {
+            sorted.add(applicant);
+        }
+
+        return sorted;
+    }
+
+    private void quickSortThreeWay(Applicant[] array, int low, int high) {
+        if (high <= low)
+            return;
+
+        int lt = low, gt = high;
+        Applicant pivot = array[low];
+        int i = low;
+
+        while (i <= gt) {
+            int cmp = array[i].compareTo(pivot);
+
+            if (cmp < 0) {
+                // array[i] < pivot
+                swap(array, lt++, i++);
+            } else if (cmp > 0) {
+                // array[i] > pivot (lower risk score)
+                swap(array, i, gt--);
+            } else {
+                i++;
+            }
+        }
+
+        quickSortThreeWay(array, low, lt - 1);
+        quickSortThreeWay(array, gt + 1, high);
+    }
+
+    private void swap(Applicant[] array, int i, int j) {
+        Applicant temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
 }
